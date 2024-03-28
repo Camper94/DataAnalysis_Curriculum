2ad648d1eb906bb416e241b88764b2b261d32ce8
Sofiane <sofianeboumelit@protonmail.ch>
2024-03-28 23:40:27 +0100

    wip

--- Data-Driven Decision Making in SQL/Chapter_Two.sql
+++ Data-Driven Decision Making in SQL/Chapter_Two.sql
@@ -253,3 +253,121 @@ WHERE movie_id =
     FROM renting
     GROUP BY movie_id
     HAVING AVG(rating)>9);
+/*Groups of customers
+Use the CUBE operator to extract the content of a pivot table from the database.
+Create a table with the total number of male and female customers from each country.*/
+SELECT gender, -- Extract information of a pivot table of gender and country for the number of customers
+	   country,
+	   COUNT(*)
+FROM customers
+GROUP BY CUBE (gender, country)
+ORDER BY country;
+/*Categories of movies
+Give an overview on the movies available on MovieNow. 
+List the number of movies for different genres and release years.*/
+SELECT genre,
+       year_of_release,
+       COUNT(*)
+FROM movies
+GROUP BY CUBE(genre,year_of_release)
+ORDER BY year_of_release;
+/*Analyzing average ratings
+Prepare a table for a report about the national preferences of the customers from MovieNow comparing 
+the average rating of movies across countries and genres.*/
+SELECT 
+	country, 
+	genre, 
+	AVG(r.rating) AS avg_rating 
+FROM renting AS r
+LEFT JOIN movies AS m
+ON m.movie_id = r.movie_id
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+GROUP BY CUBE(country,genre);
+/*Number of customers
+You have to give an overview of the number of customers for a presentation.*/
+SELECT country,
+       gender,
+	   COUNT(*)
+FROM customers
+GROUP BY ROLLUP (country, gender)
+ORDER BY country, gender;
+/*Analyzing preferences of genres across countries
+You are asked to study the preferences of genres across countries.
+Are there particular genres which are more popular in specific countries? 
+Evaluate the preferences of customers by averaging their ratings and counting 
+the number of movies rented from each genre.*/
+SELECT 
+	c.country, 
+	m.genre, 
+	AVG(r.rating) AS avg_rating, 
+	COUNT(*) AS num_rating
+FROM renting AS r
+LEFT JOIN movies AS m
+ON m.movie_id = r.movie_id
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+GROUP BY ROLLUP (c.country,m.genre)
+ORDER BY c.country, m.genre;
+/*Exploring nationality and gender of actors
+For each movie in the database, 
+the three most important actors are identified and listed in the table actors. 
+This table includes the nationality and gender of the actors. 
+We are interested in how much diversity there is in the nationalities of the actors 
+and how many actors and actresses are in the list.*/
+SELECT 
+	nationality, 
+    gender, 
+    COUNT(*) 
+FROM actors
+GROUP BY GROUPING SETS ((nationality), (gender), ());
+/*Exploring rating by country and gender
+Now you will investigate the average rating of customers aggregated by country and gender.*/
+SELECT 
+	c.country, 
+    c.gender,
+	AVG(r.rating)
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+-- Report all info from a Pivot table for country and gender
+GROUP BY GROUPING SETS ((country, gender),(country),(gender),());
+/*Customer preference for genres
+You just saw that customers have no clear preference for more recent movies over older ones. 
+Now the management considers investing money in movies of the best rated genres.*/
+SELECT genre,
+	   AVG(rating) AS avg_rating,
+	   COUNT(rating) AS n_rating,
+       COUNT(*) AS n_rentals,     
+	   COUNT(DISTINCT m.movie_id) AS n_movies 
+FROM renting AS r
+LEFT JOIN movies AS m
+ON m.movie_id = r.movie_id
+WHERE r.movie_id IN ( 
+	SELECT movie_id
+	FROM renting
+	GROUP BY movie_id
+	HAVING COUNT(rating) >= 3 )
+AND r.date_renting >= '2018-01-01'
+GROUP BY genre
+ORDER BY avg_rating DESC;
+/*Customer preference for actors
+The last aspect you have to analyze are customer preferences for certain actors.*/
+SELECT a.nationality,
+       a.gender,
+	   AVG(r.rating) AS avg_rating,
+	   COUNT(r.rating) AS n_rating,
+	   COUNT(*) AS n_rentals,
+	   COUNT(DISTINCT a.actor_id) AS n_actors
+FROM renting AS r
+LEFT JOIN actsin AS ai
+ON ai.movie_id = r.movie_id
+LEFT JOIN actors AS a
+ON ai.actor_id = a.actor_id
+WHERE r.movie_id IN ( 
+	SELECT movie_id
+	FROM renting
+	GROUP BY movie_id
+	HAVING COUNT(rating) >= 4)
+AND r.date_renting >= '2018-04-01'
+GROUP BY GROUPING SETS ((a.nationality,a.gender),(a.nationality),(a.gender),());
