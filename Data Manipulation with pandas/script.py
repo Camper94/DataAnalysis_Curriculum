030f3487288b76b79047a6817fe903c18d934d30
Sofiane B <56474189+Camper94@users.noreply.github.com>
2024-07-04 23:00:29 +0100

    Update script.py

--- Data Manipulation with pandas/script.py
+++ Data Manipulation with pandas/script.py
@@ -230,3 +230,38 @@ high_homelessness_srt = high_homelessness.sort_values("indiv_per_10k",ascending 
 result = high_homelessness_srt[["state","indiv_per_10k"]]
 # See the result
 print(result)
+
+#Mean and Median
+'''
+Summary statistics are exactly what they sound like - 
+they summarize many numbers in one statistic. 
+For example, mean, median, minimum, maximum, and standard deviation are summary statistics.
+Calculating summary statistics allows you to get a better sense of your data, 
+even if there's a lot of it.
+sales is available and pandas is loaded as pd.
+'''
+
+# Print the head of the sales DataFrame
+print(sales.head())
+# Print the info about the sales DataFrame
+print(sales.info())
+# Print the mean of weekly_sales
+print(sales["weekly_sales"].mean())
+# Print the median of weekly_sales
+print(sales["weekly_sales"].median())
+
+#Summarizing dates
+'''
+Summary statistics can also be calculated on date columns that have values with the data
+type datetime64. Some summary statistics — like mean — don't make a ton of sense on dates, 
+but others are super helpful, for example, minimum and maximum, 
+which allow you to see what time range your data covers.
+sales is available and pandas is loaded as pd.
+'''
+
+# Print the maximum of the date column
+print(sales["date"].max())
+# Print the minimum of the date column
+print(sales["date"].min())
+
+
