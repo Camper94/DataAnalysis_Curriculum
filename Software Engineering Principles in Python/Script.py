439bbcbded92034f52b6189ee596eb7dda6c952d
Sofiane B <56474189+Camper94@users.noreply.github.com>
2024-07-23 02:55:48 +0100

    Update Script.py

--- Software Engineering Principles in Python/Script.py
+++ Software Engineering Principles in Python/Script.py
@@ -208,3 +208,121 @@ text_analyzer
 ├── __init__.py
 └── counter_utils.py
 '''
+# Import needed functionality
+from collections import Counter
+def plot_counter(counter, n_most_common=5):
+  # Subset the n_most_common items from the input counter
+  top_items = counter.most_common(n_most_common)
+  # Plot `top_items`
+  plot_counter_most_common(top_items)
+# Import needed functionality
+from collections import Counter
+def sum_counters(counters):
+  # Sum the inputted counters
+  return sum(counters, Counter())
+
+'''
+Question
+You just wrote two functions for your package in the file counter_utils.py named plot_counter & sum_counters. 
+Which of the following lines would correctly import these functions in __init__.py using relative import syntax?
+
+Possible answers
+
+from counter_utils import plot_counter, sum_counters()
+from .counter_utils import plot_counter, sum_counters(X)
+from . import plot_counter, sum_counters()
+from .counter_utils import plot_counter & sum_counters()
+'''
+
+#Using your package's new functionality
+'''
+You've now created some great functionality for text analysis to your package. 
+In this exercise, you'll leverage your package to analyze some tweets written by DataCamp & DataCamp users.
+
+The object word_counts is loaded into your environment. 
+It contains a list of Counter objects that contain word counts from a sample of DataCamp tweets.
+
+The structure you've created can be seen in the tree below. You'll be working in my_script.py.
+
+working_dir
+├── text_analyzer
+│    ├── __init__.py
+│    ├── counter_utils.py
+└── my_script.py
+'''
+
+# Import local package
+import text_analyzer as ta
+# Sum word_counts using sum_counters from text_analyzer
+word_count_totals = ta.sum_counters(word_counts)
+# Plot word_count_totals using plot_counter from text_analyzer
+ta.plot_counter(word_count_totals)
+
+#Writing requirements.txt
+'''
+We covered how having a requirements.txt file can help your package be more portable by allowing your users to easily 
+recreate its intended environment. In this exercise, you will be writing the contents of a 
+requirements file to a python variable.
+Note, in practice, the code you write in this exercise would 
+be written to it's own txt file instead of a variable in your python session.
+'''
+
+requirements = """
+matplotlib>=3.0.0
+numpy==1.15.4
+pandas<=0.22.0
+pycodestyle
+"""
+
+#Installing package requirements
+'''
+You've now written a requirements.txt file to recreate your package's environment using a pip install command. 
+Given that you are running a shell session in the work_dir structure shown below, 
+what command would properly recreate the my_package environment from requirements.txt?
+
+work_dir/
+├── my_package
+│&nbsp;&nbsp; ├── __init__.py
+│&nbsp;&nbsp; └── utils.py
+├── requirements.txt
+└── setup.py
+
+Possible Answers
+Select one answer
+
+-pip install requirements.txt()
+-pip install -r requirements()
+-pip install -r setup.py()
+-pip install -r requirements.txt(X)
+'''
+
+#Creating setup.py
+'''
+In order to make your package installable by pip you need to create a setup.py file. 
+In this exercise you will create this file for the text_analyzer package you've been building.
+'''
+
+# Import needed function from setuptools
+from setuptools import setup
+# Create proper setup to be used by pip
+setup(name='text_analyzer',
+      version='0.0.1',
+      description='Perform and visualize a text anaylsis.',
+      author='Sofiane Boumelit',
+      packages=['text_analyzer'])
+
+#Listing requirements in setup.py
+'''
+We created a setup.py file earlier, 
+but we forgot to list our dependency on matplotlib in the install_requires argument. 
+In this exercise you will practice listing your version specific dependencies by correcting the setup.py 
+you previously wrote for your text_analyzer package.
+'''
+
+# Create proper setup to be used by pip
+setup(name='text_analyzer',
+      version='0.0.1',
+      description='Perform and visualize a text anaylsis.',
+      author='Sofiane Boumelit',
+      packages=['text_analyzer'],
+      install_requires=['matplotlib>=3.0.0'])
